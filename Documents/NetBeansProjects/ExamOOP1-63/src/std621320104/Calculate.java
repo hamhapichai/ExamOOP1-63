cc012921139e941d681986393ab4f5b5bcd6907d
hamhapichai <hamhapichai@gmail.com>
2021-01-31 19:52:34 +0700

    ExamOOP1-63

--- Documents/NetBeansProjects/ExamOOP1-63/src/std621320104/Calculate.java
+++ Documents/NetBeansProjects/ExamOOP1-63/src/std621320104/Calculate.java
@@ -1,9 +1,23 @@
 package std621320104;
-public class Calculate {
+import java.util.Random;
+public class Calculate{
         public void plus(int x,int y){ int z = x + y;}
         public void minus(int x,int y){ int z = x - y;}
+}
+
+class Randomm{
+    public void rand(){
+        Random rand = new Random();
         
-        static class Random{
-            
+        for(int i=0;i<20;i++){
+            System.out.println(rand.nextInt(9));
         }
     }
+}
+
+class Run{
+    public static void main(String[] args) {
+        Randomm p2 = new Randomm();
+        p2.rand();
+    }
+}
